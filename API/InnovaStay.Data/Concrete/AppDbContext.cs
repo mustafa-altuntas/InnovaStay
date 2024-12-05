79cdebc53ef7dfd8b4eb3a9a32dabd8035ef6a30
mustafa altuntaş <altun_tas@outlook.com>
2024-12-05 09:02:16 +0300

    Add project files.

---
+++ API/InnovaStay.Data/Concrete/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using InnovaStay.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Data.Concrete
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-EL2OSRJ\\SQLEXPRESS;Initial Catalog=InnovaStayDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
+    }
+}
---
+++ UI/InnovaStay.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InnovaStay.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
