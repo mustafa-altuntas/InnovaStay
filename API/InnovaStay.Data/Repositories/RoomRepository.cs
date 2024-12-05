e7066fbfe807e81fde46da219ceeaab96da79888
mustafa altuntaş <altun_tas@outlook.com>
2024-12-05 10:48:28 +0300

    feat: Implement RoomRepository class based on GenericRepository

---
+++ API/InnovaStay.Data/Repositories/RoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using InnovaStay.Data.Abstract;
+using InnovaStay.Data.Concrete;
+using InnovaStay.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Data.Repositories
+{
+    public class RoomRepository : GenericRepository<Room>, IRoomRepository
+    {
+        public RoomRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+
+    }
+}
