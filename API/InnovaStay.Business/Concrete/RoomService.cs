9642a0cfa2cc79a4234d557a5458ad256f9e8b1c
mustafa altuntaş <altun_tas@outlook.com>
2024-12-09 22:38:09 +0300

    ✨ Add Implement RoomService based on GenericService

---
+++ API/InnovaStay.Business/Concrete/RoomService.cs
@@ -0,0 +1,18 @@
+﻿using InnovaStay.Data.Abstract;
+using InnovaStay.Dto.Dtos.Room;
+using InnovaStay.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Business.Concrete
+{
+    public class RoomService : GenericService<RoomDto, Room>
+    {
+        public RoomService(IGenericRepository<Room> repository) : base(repository)
+        {
+        }
+    }
+}
