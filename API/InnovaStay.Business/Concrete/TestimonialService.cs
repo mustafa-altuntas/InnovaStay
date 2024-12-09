896cf4f8eea23cb5ec925f8e0926e30260267578
mustafa altuntaş <altun_tas@outlook.com>
2024-12-09 22:40:33 +0300

    ✨ Add Implement TestimonialService based on GenericService

---
+++ API/InnovaStay.Business/Concrete/TestimonialService.cs
@@ -0,0 +1,18 @@
+﻿using InnovaStay.Data.Abstract;
+using InnovaStay.Dto.Dtos.Testimonial;
+using InnovaStay.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Business.Concrete
+{
+    public class TestimonialService : GenericService<TestimonialDto, Testimonial>
+    {
+        public TestimonialService(IGenericRepository<Testimonial> repository) : base(repository)
+        {
+        }
+    }
+}
