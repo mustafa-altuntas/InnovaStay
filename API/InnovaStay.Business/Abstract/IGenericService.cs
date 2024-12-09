1b63f76e106c73b830cf621ad164143a5e16264d
mustafa altuntaş <altun_tas@outlook.com>
2024-12-09 15:36:30 +0300

    feat: Define IGenericService interface for business layer

---
+++ API/InnovaStay.Business/Abstract/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Business.Abstract
+{
+    public interface IGenericService<TDto> where TDto : class
+    {
+        Task<TDto?> GetByIdAsync(int id);
+        Task<IEnumerable<TDto>> GetAllAsync();
+        Task AddAsync(TDto dto);
+        void Update(TDto dto);
+        void Remove(TDto dto);
+
+
+    }
+}
