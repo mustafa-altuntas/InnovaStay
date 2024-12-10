a47c2fa78a3d9d7c2fc354f4782b48563c2b0922
mustafa altuntaş <altun_tas@outlook.com>
2024-12-10 09:50:07 +0300

    ✨ Add implement ISubscribeService class based on IGenericService

---
+++ API/InnovaStay.Business/Abstract/ISubscribeService.cs
@@ -0,0 +1,15 @@
+﻿using InnovaStay.Dto.Dtos.Subscribe;
+using InnovaStay.Dto.Dtos.Testimonial;
+using InnovaStay.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Business.Abstract
+{
+    public interface ISubscribeService : IGenericService<SubscribeDto,Subscribe>
+    {
+    }
+}
