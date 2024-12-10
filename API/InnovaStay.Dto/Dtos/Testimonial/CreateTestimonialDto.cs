cd5ce7df07cec32e9aaba68030c1eb4de3d20cfa
mustafa altuntaş <altun_tas@outlook.com>
2024-12-10 12:09:20 +0300

    ✨ Created Create and Update DTOs for entities

---
+++ API/InnovaStay.Dto/Dtos/Testimonial/CreateTestimonialDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Dto.Dtos.Testimonial
+{
+    public class CreateTestimonialDto
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
