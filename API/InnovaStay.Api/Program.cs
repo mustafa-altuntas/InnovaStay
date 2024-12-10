8a707a318df6c41705e086500e7c7526240ced16
mustafa altuntaş <altun_tas@outlook.com>
2024-12-10 11:56:04 +0300

    🔧 Registered services and repositories in dependency injection container for scoped lifetime

--- API/InnovaStay.Api/Program.cs
+++ API/InnovaStay.Api/Program.cs
@@ -1,6 +1,34 @@
+using InnovaStay.Business.Abstract;
+using InnovaStay.Business.Concrete;
+using InnovaStay.Data.Abstract;
+using InnovaStay.Data.Concrete;
+using InnovaStay.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<,>), typeof(GenericService<,>));
+
+
+
+builder.Services.AddScoped<IRoomRepository,RoomRepository>();
+builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<IStaffRepository, StaffRepository>();
+builder.Services.AddScoped<ISubscribeRepository, SubscribeRepository>();
+builder.Services.AddScoped<ITestimonialRepository, TestimonialRepository>();
+
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<IServiceService, ServiceService>();
+builder.Services.AddScoped<IStaffService, StaffService>();
+builder.Services.AddScoped<ISubscribeService, SubscribeService>();
+builder.Services.AddScoped<ITestimonialService, TestimonialService>();
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
