d1b6039acbb8cf890c7ca8bd196f126d3514a56e
mustafa altuntaş <2altuntas9@gmail.com>
2025-01-12 15:34:13 +0300

    .

--- API/InnovaStay.Data/Concrete/AppDbContext.cs
+++ API/InnovaStay.Data/Concrete/AppDbContext.cs
@@ -12,7 +12,7 @@ namespace InnovaStay.Data.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-EL2OSRJ\\SQLEXPRESS;Initial Catalog=InnovaStayDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-EL2OSRJ\\SQLEXPRESS;Initial Catalog=InnovaStayDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
         }
 
         public DbSet<Room> Rooms { get; set; }
