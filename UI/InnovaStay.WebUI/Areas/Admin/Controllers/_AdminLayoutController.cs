e1569d28a3544245ae4f9c894ce6ee0ef61d54dd
mustafa altuntaş <2altuntas9@gmail.com>
2025-01-12 21:28:50 +0300

    .

---
+++ UI/InnovaStay.WebUI/Areas/Admin/Controllers/_AdminLayoutController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InnovaStay.WebUI.Areas.Admin.Controllers
+{
+    public class _AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
