af2132048aea33e9fcef4ba514093e5c685e54eb
mustafa altuntaş <2altuntas9@gmail.com>
2025-01-12 15:33:48 +0300

    .

---
+++ API/InnovaStay.Data/ServiceRegistiration.cs
@@ -0,0 +1,24 @@
+﻿using InnovaStay.Data.Concrete;
+using InnovaStay.Entity.Constant;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Data
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddDataService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opts =>
+            {
+                opts.UseNpgsql(configuration.GetConnectionString(AppsettingConstants.PostgreDb));
+            });
+        }
+    }
+}
