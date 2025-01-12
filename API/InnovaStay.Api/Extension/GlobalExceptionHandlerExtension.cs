7a1fe7bcd69dc8a8f61b5c60c25debad323890fa
mustafa altuntaş <2altuntas9@gmail.com>
2025-01-12 15:32:31 +0300

    .

---
+++ API/InnovaStay.Api/Extension/GlobalExceptionHandlerExtension.cs
@@ -0,0 +1,34 @@
+﻿using InnovaStay.Business.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net.Mime;
+
+namespace InnovaStay.Api.Extension
+{
+    public static class GlobalExceptionHandlerExtension
+    {
+        public static void ExceptionHandler(this WebApplication app)
+        {
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    var exceptionHandlerFactory = context.Features.Get<IExceptionHandlerFeature>();
+
+                    int statusCode = exceptionHandlerFactory.Error switch
+                    {
+                        EntityNotFoundException => 404,
+                        _ => 500
+                    };
+
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    context.Response.StatusCode = statusCode;
+                    await context.Response.WriteAsync(exceptionHandlerFactory.Error.Message);
+
+
+
+                });
+            });
+        }
+
+    }
+}
