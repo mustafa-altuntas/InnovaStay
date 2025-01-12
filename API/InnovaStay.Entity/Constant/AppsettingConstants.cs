fd21d701f352dc815cd27520a6e04e394aa7e10b
mustafa altuntaş <2altuntas9@gmail.com>
2025-01-12 15:33:25 +0300

    .

---
+++ API/InnovaStay.Entity/Constant/AppsettingConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnovaStay.Entity.Constant
+{
+    public class AppsettingConstants
+    {
+        public const string PostgreDb = "PostgreDb";
+    }
+}
