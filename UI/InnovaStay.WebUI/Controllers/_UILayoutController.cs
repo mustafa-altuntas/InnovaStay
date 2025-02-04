c9ee83dbcb56986a31f09ef6574060643f3bf586
mustafa-altuntas <2altuntas9@gmail.com>
2025-02-04 23:52:17 +0300

    .

---
+++ UI/InnovaStay.WebUI/Controllers/_UILayoutController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InnovaStay.WebUI.Controllers
+{
+    public class _UILayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+ 
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+ 
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
