82fe8aac0ab6aff10faa1d5ef988210de4470aa4
mustafa-altuntas <2altuntas9@gmail.com>
2025-02-05 10:11:18 +0300

    .

---
+++ API/InnovaStay.Data/Abstract/IAboutRepository.cs
@@ -0,0 +1,9 @@
+﻿using InnovaStay.Entity.Concrete;
+
+namespace InnovaStay.Data.Abstract
+{
+    public interface IAboutRepository : IGenericRepository<About>
+    {
+        Task<bool> MarkAsActivate(int id);
+    }
+}
