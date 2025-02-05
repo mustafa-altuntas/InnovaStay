40e977443309fe42e451d53ab4931d90df7b6247
mustafa-altuntas <2altuntas9@gmail.com>
2025-02-05 10:11:12 +0300

    .

--- API/InnovaStay.Data/Concrete/AppDbContext.cs
+++ API/InnovaStay.Data/Concrete/AppDbContext.cs
@@ -16,6 +16,7 @@ namespace InnovaStay.Data.Concrete
         public DbSet<Staff> Staffs { get; set; }
         public DbSet<Subscribe> Subscribes { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<About> Abouts { get; set; }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
